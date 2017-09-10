9cd0b5df6c4aa49bad3569f846c365c243a014f5
liufeiSAP <344501823@qq.com>
2017-09-10 21:21:53 +0800

    Revert "Revert "Revert "fsdfsd"""
    This reverts commit 861d875206b235f400988cff51a7f1f3709462e2.

--- src/main/java/com/deppon/test04/dao/impl/PersonDao.java
+++ src/main/java/com/deppon/test04/dao/impl/PersonDao.java
@@ -7,8 +7,6 @@ public class PersonDao implements IPersonDao {
 	public void save() {
 		System.out.println("------------from PersonDao.save()");
 		System.out.println("------------from PersonDao.save()");
-		
-		System.out.println("------------from PersonDao.save()");
 
 	}
 
